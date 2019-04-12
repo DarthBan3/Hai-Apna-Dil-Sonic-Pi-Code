066e8d6139588454c5c775f9a6bfd225fbb29e1b
DarthBan3 <49557031+DarthBan3@users.noreply.github.com>
2019-04-12 16:44:22 +0530

    Add files via upload

---
+++ Hai Apna Dil.rb
@@ -0,0 +1,199 @@
+#Hai Apna Dil Toh awara
+#Singer: Hemant Kumar
+#Music: SD Burman
+#Movie: Solva Saal (1958) {Seventeenth Year}
+#Language: Hindi
+#Code written by Sasi Mitra
+#Original Song: https://www.youtube.com/watch?v=3K8TH8LBs-w
+
+use_synth :chiplead
+#It doesn't matter which synth you use. It's upto your preference.
+
+#Chorus:
+play :C5, sustain: 0.5
+sleep 0.25
+play :F5, sustain: 0.25
+sleep 0.25
+play :F5, sustain: 0.25
+sleep 0.25
+play :G5, sustain: 0.75
+sleep 0.25
+play :G5, sustain: 0.75
+sleep 0.25
+play :A5, sustain: 1.25
+sleep 0.75
+play :F5, sustain: 1.5
+sleep 0.25
+play :G5, sustain: 0.75
+sleep 0.5
+play :F5, sustain: 0.75
+sleep 0.5
+play :E5, sustain: 0.75
+sleep 1.0
+
+play :E5, sustain: 0.5
+sleep 0.5
+play :C5, sustain: 0.75
+sleep 0.5
+play :D5, sustain: 0.75
+sleep 0.5
+play :E5, sustain: 0.75
+sleep 0.25
+play :F5, sustain: 0.5
+sleep 0.25
+play :F5, sustain: 0.5
+sleep 0.25
+play :G5, sustain: 0.25
+sleep 0.5
+play :A5, sustain: 0.25
+sleep 0.25
+play :F5, sustain: 0.25
+sleep 0.25
+play :G5, sustain: 0.25
+sleep 0.25
+play :E5, sustain: 0.25
+sleep 0.25
+play :F5, sustain: 0.5
+sleep 1.5
+
+#Harmonica Solo
+2.times do
+  play :f6, sustain: 0.20
+  sleep 0.20
+  2.times do
+    play :g6, sustain: 0.20
+    sleep 0.20
+    play :a6, sustain: 0.20
+    sleep 0.20
+  end
+  play :g6, sustain: 0.20
+  sleep 0.20
+  play :a6, sustain: 0.5
+  sleep 1.0
+  play :g6, sustain: 0.20
+  sleep 0.20
+  play :f6, sustain: 0.20
+  sleep 0.20
+  2.times do
+    play :c6, sustain: 0.20
+    sleep 0.20
+    play :d6, sustain: 0.20
+    sleep 0.20
+  end
+  play :c6, sustain: 0.5
+  sleep 1.0
+  
+  play :e6, sustain: 0.20
+  sleep 0.20
+  2.times do
+    play :f6, sustain: 0.20
+    sleep 0.20
+    play :g6, sustain: 0.20
+    sleep 0.20
+  end
+  play :f6, sustain: 0.20
+  sleep 0.20
+  play :g6, sustain: 0.5
+  sleep 1.0
+  
+  play :a6, sustain: 0.5
+  sleep 0.45
+  play :g6, sustain: 0.5
+  sleep 0.45
+  play :f6, sustain: 0.5
+  sleep 0.5
+  play :a6, sustain: 1.5
+  
+  sleep 2
+end
+
+#Main Verse
+2.times do
+  5.times do
+    play :d5, sustain: 0.25
+    sleep 0.25
+  end
+  play :c5, sustain: 0.25
+  sleep 0.25
+  play :d5, sustain: 0.25
+  sleep 0.5
+end
+
+5.times do
+  play :e5, sustain: 0.25
+  sleep 0.25
+end
+play :d5, sustain: 0.25
+sleep 0.25
+play :e5, sustain: 0.25
+sleep 0.5
+
+play :a5, sustain: 0.25
+sleep 0.25
+play :g5, sustain: 0.25
+sleep 0.25
+2.times do
+  play :f5, sustain: 0.25
+  sleep 0.25
+end
+2.times do
+  play :e5, sustain: 0.25
+  sleep 0.25
+end
+
+sleep 1.5
+
+#Chorus Repeat
+2.times do
+  play :C5, sustain: 0.5
+  sleep 0.25
+  play :F5, sustain: 0.25
+  sleep 0.25
+  play :F5, sustain: 0.25
+  sleep 0.25
+  play :G5, sustain: 0.75
+  sleep 0.25
+  play :G5, sustain: 0.75
+  sleep 0.25
+  play :A5, sustain: 1.25
+  sleep 0.75
+  play :F5, sustain: 1.5
+  sleep 0.25
+  play :G5, sustain: 0.75
+  sleep 0.5
+  play :F5, sustain: 0.75
+  sleep 0.5
+  play :E5, sustain: 0.75
+  sleep 1.0
+  
+  play :E5, sustain: 0.5
+  sleep 0.5
+  play :C5, sustain: 0.75
+  sleep 0.5
+  play :D5, sustain: 0.75
+  sleep 0.5
+  play :E5, sustain: 0.75
+  sleep 0.25
+  play :F5, sustain: 0.5
+  sleep 0.25
+  play :F5, sustain: 0.5
+  sleep 0.25
+  play :G5, sustain: 0.25
+  sleep 0.5
+  play :A5, sustain: 0.25
+  sleep 0.25
+  play :F5, sustain: 0.25
+  sleep 0.25
+  play :G5, sustain: 0.25
+  sleep 0.25
+  play :E5, sustain: 0.25
+  sleep 0.25
+  play :F5, sustain: 0.5
+  sleep 1.5
+end
+
+
+
+
+
+
